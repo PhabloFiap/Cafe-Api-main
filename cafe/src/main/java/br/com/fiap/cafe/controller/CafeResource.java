273cf56ef98cb28447ec3a5e32a3b55f75772ae2
PhabloFiap <rm550687@fiap.com.br>
2023-10-19 22:48:25 -0300

    post feito (arrumar id nextval e arrumar not null)

--- cafe/src/main/java/br/com/fiap/cafe/controller/CafeResource.java
+++ cafe/src/main/java/br/com/fiap/cafe/controller/CafeResource.java
@@ -1,21 +1,26 @@
 package br.com.fiap.cafe.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import br.com.fiap.cafe.model.Cafe;
 import br.com.fiap.cafe.model.repository.CafeRepository;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.ResponseBuilder;
+import jakarta.ws.rs.core.UriBuilder;
 
 @Path("/cafe")
 public class CafeResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	
 	public Response findAll() {
 
 		List<Cafe> retorno = CafeRepository.findAll();
@@ -25,4 +30,20 @@ public class CafeResource {
 
 	}
 
+	@POST
+	//@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response salva (Cafe cafe) {
+		
+		Cafe resp = CafeRepository.salva(cafe);
+		
+		final URI cafeUri = UriBuilder.fromResource(CafeResource.class).path("/cafe/{id}").build(resp.getId());
+		
+		
+		ResponseBuilder response = Response.created(cafeUri);
+		response.entity(resp);
+		return response.build();
+		
+	}
+	
 }
--- cafe/src/main/java/br/com/fiap/cafe/model/repository/CafeRepository.java
+++ cafe/src/main/java/br/com/fiap/cafe/model/repository/CafeRepository.java
@@ -1,5 +1,7 @@
 package br.com.fiap.cafe.model.repository;
 
+import java.sql.CallableStatement;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -27,6 +29,7 @@ public class CafeRepository extends Repository {
 					Cafe cafe = new Cafe();
 					cafe.setId(rs.getLong("ID"));
 					cafe.setNome(rs.getString("nome"));
+					cafe.setPreco(rs.getDouble("preco"));
 					cafe.setDataFabricacao(rs.getDate("dataFabricacao").toLocalDate());
 					cafe.setDataValidade(rs.getDate("dataValidade").toLocalDate());
 
@@ -43,6 +46,42 @@ public class CafeRepository extends Repository {
 		}
 
 		return cafes;
+	}
+
+	public static Cafe salva(Cafe cafe) {
+
+		String sql = "begin insert into cafe (id, nome, preco, dataFabricacao, dataValidade ) values(SQ_CAFE.nextval,?,?,?,?)"
+				+ "return id into ?; end;";
+
+		CallableStatement cs = null;
+
+		try {
+			cs = getConnection().prepareCall(sql);
+			cs.setString(1, cafe.getNome());
+			cs.setDouble(2, cafe.getPreco());
+			cs.setDate(3, Date.valueOf(cafe.getDataFabricacao()));
+			cs.setDate(4, Date.valueOf(cafe.getDataValidade()));
+
+			cs.registerOutParameter(5, java.sql.Types.INTEGER);
+			cs.executeUpdate();
+			cafe.setId(Long.valueOf(cs.getInt(5)));
+
+			return cafe;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("erro ao salvar " + e.getMessage());
+		} finally {
+			if (cs != null)
+				try {
+					cs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+
+		return null;
 	}
 
 }
